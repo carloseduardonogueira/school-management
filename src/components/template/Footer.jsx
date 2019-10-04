c769f7e8472401acba5d40c1df077a47f3c39b26
carloseduardonogueira <cenogueira3@gmail.com>
2019-10-03 23:31:50 -0300

    001-Creating project structure

---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css';
+import React from 'react';
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido para a disciplina 
+            <strong> Gestão de Projetos B</strong>
+        </span>
+    </footer>
---
+++ src/components/template/Nav.jsx
@@ -0,0 +1,15 @@
+import './Nav.css';
+import React from 'react';
+
+ export default props =>
+    <aside className="menu-area">
+        <nav className="menu">
+            {/*Refatorar e criar um componente*/}
+            <a href="#/">
+                <i className="fa fa-home"></i>Home
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i>Users
+            </a>
+        </nav>
+    </aside>
