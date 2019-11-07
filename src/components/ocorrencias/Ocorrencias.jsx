03233a8c1f0f7b913e18dcd8d69e85aba8e2b11f
Henrique <henrique9811@hotmail.com>
2019-11-07 09:45:03 -0300

    metodos para ocorrencia feitos

--- src/components/ocorrencias/Ocorrencias.jsx
+++ src/components/ocorrencias/Ocorrencias.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Main from '../template/Main';
 import axios from 'axios';
+import Materia from '../materias/Materias';
 
 const headerProps = {
     icon: 'users',
@@ -9,8 +10,109 @@ const headerProps = {
   };
 
   const baseUrl = 'http://localhost:3001/ocorrencias';
+  const InitialState = {
+    ocorrencia: { name:'' , aluno:' ', imagem: null},
+    alunos:[],
+    list:[],
+    isInvalid : false,
+    saved : false,
+    isEmpty : true
+  }
   
   export default class Ocorrência extends Component{
+
+    state = { ...InitialState }
+
+    componentWillMount(){
+      axios(baseUrl).then(ocorrencia => {
+        axios("http://localhost:3001/alunos").then(alunos => {
+          this.setState({ alunos: alunos.data, list: ocorrencia.data })
+        });
+      })
+    }
+
+    load(ocorrencia) {
+      this.setState({ ocorrencia })
+    }
+
+    clear() {
+      this.setState({ state: this.initialState });
+    }
+
+    save() {
+      const ocorrencia = this.state.ocorrencia;
+      const method = ocorrencia.id ? 'put' : 'post'
+      const url = ocorrencia.id ? `${baseUrl}/${ocorrencia.id}` : baseUrl
+      axios[method](url, ocorrencia).then(res => {
+        const list = this.getUpdatedList(res.data)
+        this.setState({ saved: true });
+        setTimeout(() => { this.setState({ ocorrencia: InitialState, isInvalid: true, saved: false }) }, 1000);
+      });
+    }
+
+    getUpdatedList(ocorrencia) {
+      const list = this.state.list.filter(o => o.id != ocorrencia.id)
+      list.unshift(ocorrencia);
+      return list;
+    }
+
+    updateField(event){
+      const ocorrencia = { ...this.state.ocorrencia }
+      ocorrencia[event.target.name] = event.target.value;
+      let isEmpty = false;
+      if(ocorrencia.name === '' || ocorrencia.aluno === '' ) 
+      isEmpty = true;
+      this.setState({ocorrencia, isEmpty});
+    }
+
+    fileSelect(event){
+      const ocorrencia = {... this.state.ocorrencia}
+      ocorrencia[event.target.name] = event.target.files[0];
+      console.log(ocorrencia[event.target.name]);
+      let isEmpty = false;
+      if(ocorrencia.imagem == null)
+      isEmpty = true;
+      this.setState({ocorrencia, isEmpty})
+    }
+
+    remove(ocorrencia) {
+      if (window.confirm("Deseja realmente excluir esta ocorrência?")) {
+        axios.delete(`${baseUrl}/${ocorrencia.id}`).then(res => {
+          const list = this.state.list.filter(o => o !== ocorrencia)
+          this.setState({ list })
+        })
+      }
+    }
+
+    renderOptions() {
+      return this.state.alunos.map(alunos => {
+        return (
+          <option>{alunos.name}</option>
+        )
+      })
+    }
+
+    renderRows() {
+      return this.state.list.map(ocorrencia => {
+        return (
+          <tr key={ocorrencia.id}>
+            <td>{ocorrencia.name}</td>
+            <td>{ocorrencia.aluno}</td>
+            <td>{ocorrencia.imagem}</td>
+            <td>
+              <button className='btn btn-warning'
+                onClick = {() => this.load(ocorrencia)}> 
+                <i className='fa fa-pencil'></i>
+              </button>
+              {/* <button className='btn btn-danger ml-2'
+                onClick = {() => this.remove(ocorrencia)}> 
+                <i className='fa fa-trash'></i>
+              </button> */}
+            </td>
+          </tr>
+        )
+      })
+    }
 
    renderForm(){
        return(
@@ -20,14 +122,16 @@ const headerProps = {
                        <div className="col-12 col-md-6">
                            <div className="form-group">
                                 <label for = "ocorrencia">Ocorrência:</label>
-                                <input type = "text" className = "form-control" name = "ocorrencia" placeholder = "digite a ocorrência"/>
+                                <input type = "text" className = "form-control" name = "ocorrencia" placeholder = "digite a ocorrência" onChange = { e => this.updateField(e) }/>
                             </div>
                        </div> 
                        <div className = "col-12 col-md-6">
                            <div className = "form-group">
                                <label for = "aluno">Aluno</label>
-                               <select type = "select" className = "form-control"  name = "aluno">
+                               <select type = "select" className = "form-control"  name = "aluno" onChange = {e => this.updateField(e)}>
                                <option selected disabled>Selecione o aluno</option>
+                               <option>teste</option>
+                               {this.renderOptions()}
                                </select>
                            </div>
                        </div>
@@ -37,7 +141,7 @@ const headerProps = {
                        <div className = "col-12 col-md-6">
                            <div className = "from-group">
                                <label for = "foto"> Subir foto da Ocorrência:</label>
-                               <input type = "file" name = "foto" accept="image/*"/>
+                               <input type = "file" name = "foto" accept="image/*" onChange = {e => this.fileSelect(e)}/>
                            </div>
                        </div>
                    </div>
@@ -46,35 +150,35 @@ const headerProps = {
             <div className="col-12 d-flex justify-content-end">
               <button
                 className="btn btn-primary"
-                //onClick={e => this.save(e)}
-                //disabled={this.state.isEmpty}
+                onClick={e => this.save(e)}
+                disabled={this.state.isEmpty}
               >
                 Salvar
               </button>
 
               <button
                 className="btn btn-secondary ml-2"
-                //onClick={e => this.clear(e)}
+                onClick={e => this.clear(e)}
               >
                 Cancelar
               </button>
             </div>
             <div className="col-12 d-flex justify-content-end">
               {
-                //this.state.isEmpty && (
-                  //<div class="alert alert-danger" role="alert">
-                    //Você deve preencher os dados!
-                  //</div>
-                //)
+                this.state.isEmpty && (
+                  <div class="alert alert-danger" role="alert">
+                    Você deve preencher os dados!
+                  </div>
+                )
               }
             </ div>
             <div className="col-12 d-flex justify-content-end">
               {
-                /*this.state.saved && (
+                this.state.saved && (
                   <div class="alert alert-success" role="alert">
                     Ocorrência inserida com sucesso!
                   </div>
-                )*/
+                )
               }
             </div>
           </div>
@@ -94,7 +198,7 @@ const headerProps = {
                    </tr>
                </thead>
                <tbody>
-
+               {this.renderRows()}
                </tbody>
            </table>
        )
